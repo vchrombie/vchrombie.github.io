e813cc1f07324f9a94028fc04607fa5b9387d8ea
Venu Vardhan Reddy Tekula <venuvardhanreddytekula8@gmail.com>
2017-08-27 12:11:14 +0530

    added the required files

---
+++ vchrombie_files/script.js
@@ -0,0 +1,14 @@
+/*
+* @Author: Venu
+* @Date:   2017-06-22 10:45:00
+* @Last Modified by:   Venu
+* @Last Modified time: 2017-07-01 18:43:16
+*/
+
+$(function(){
+	$("#about-me").typed({
+		strings: ["Student at Amrita University.","Loves to Code <&#x2764>.","Web Developer.","Optimist. Foodie. Techie.","Open Source Enthusiast."],
+		typeSpeed: 30,
+		loop: true,
+	});
+});
