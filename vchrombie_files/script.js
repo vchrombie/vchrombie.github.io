e61fd4a7b51f2c2de64d06d0ea6a91a1864bb7a5
Venu Vardhan Reddy Tekula <venuvardhanreddytekula8@gmail.com>
2017-08-27 12:36:18 +0530

    Update script.js

--- vchrombie_files/script.js
+++ vchrombie_files/script.js
@@ -2,12 +2,12 @@
 * @Author: Venu
 * @Date:   2017-06-22 10:45:00
 * @Last Modified by:   Venu
-* @Last Modified time: 2017-07-01 18:43:16
+* @Last Modified time: 2017-08-27 12:13:16
 */
 
 $(function(){
 	$("#about-me").typed({
-		strings: ["Student at Amrita University.","Loves to Code <&#x2764>.","Web Developer.","Optimist. Foodie. Techie.","Open Source Enthusiast."],
+		strings: ["Student at Amrita University.","Member @am.foss","Loves to Code <&#x2764>.","Web Developer.","Optimist. Foodie. Techie.","Open Source Enthusiast."],
 		typeSpeed: 30,
 		loop: true,
 	});
