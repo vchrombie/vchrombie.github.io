c9a1034c841596595a213f0a46ea43b3528f75e9
vchrombie <venuvardhanreddytekula8@gmail.com>
2017-10-24 01:13:21 +0530

    Added js folder

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/*
+* @Author: Venu
+* @Date:   2017-06-22 10:45:00
+* @Last Modified by:   Venu
+* @Last Modified time: 2017-08-27 12:13:16
+*/
+
+$(function(){
+	$("#about-me").typed({
+		strings: ["Student at Amrita University.","Member @am.foss .","Loves to Code <&#x2764>.","Student Web Developer.","Optimist. Foodie. Techie.","a JavaScript aficionado","Open Source Enthusiast."],
+		typeSpeed: 30,
+		loop: true,
+	});
+});
