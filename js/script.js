14fc4dff0d37113e98ccf902f816b0a6974a908b
Venu Vardhan Reddy Tekula <venuvardhanreddytekula8@gmail.com>
2018-08-25 23:53:49 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@
 
 $(function(){
 	$("#about-me").typed({
-		strings: ["Student at Amrita University.","Member @amfoss.","Haiku Contributor.","Loves to Code <&#x2764>.","Open Source Enthusiast."],
+		strings: ["Student at Amrita University.","Open Source Enthusiast.","Member @amfoss.","Data Science Freak.","Web Developer.","Loves to Code <&#x2764>."],
 		typeSpeed: 30,
 		loop: true,
 	});
