e0f2468ae4cb3eb8c47fb53ac511bfa320d31847
Venu Vardhan Reddy Tekula <venuvardhanreddytekula8@gmail.com>
2023-03-20 01:12:45 -0400

    update about me

--- js/script.js
+++ js/script.js
@@ -2,13 +2,13 @@ $(function(){
 	$("#about-me").typed({
 		strings: [
 			"mscs grad student @nyutandon.",
+			"devops floodnet.nyc @nyu_cusp.",
 			"ex- @bitergia @beehyvsoftware.",
 			"undergrad from @amritaedu.",
 			"mentor @amfoss_in.",
 			"gsoc 2020 w/ @chaossproj.",
 			"data detective.",
 			"open-source enthusiast.",
-			"optimist | foodie | techie"
 		],
 		typeSpeed: 30,
 		loop: true,
