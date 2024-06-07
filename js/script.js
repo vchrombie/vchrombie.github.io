8f9e56127d0043403899036bc6d636bb5d858484
Venu Vardhan Reddy Tekula <vt2182@nyu.edu>
2024-06-06 20:36:43 -0400

    Update some styling, links

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
-$(function(){
-	$("#about-me").typed({
-		strings: [
-			"mscs grad student @nyutandon.",
-			"engineer at floodnet.nyc @nyu_cusp.",
-			"ex- @bitergia @beehyvsoftware.",
-			"undergrad from @amritaedu.",
-			"mentor @amfoss_in.",
-			"gsoc 2020 w/ @chaossproj.",
-			"data detective.",
-			"open-source enthusiast.",
-		],
-		typeSpeed: 30,
-		loop: true,
-	});
+$(function () {
+  $("#about-me").typed({
+    strings: [
+      "mscs grad student @nyutandon",
+      "engineer at floodnet.nyc @nyu_cusp",
+      "ex- @bitergia @beehyvsoftware",
+      "undergrad from @amritaedu",
+      "mentor @amfoss_in",
+      "gsoc 2020 w/ @chaossproj",
+      "data detective",
+      "open-source enthusiast",
+    ],
+    typeSpeed: 30,
+    loop: true,
+  });
 });
